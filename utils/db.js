cae643177b73460aa6ef1616d228290559d213e4
Antoniomary <amanso.totti@yahoo.com>
2024-10-16 20:27:13 +0000

    db.js

---
+++ utils/db.js
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const dbName = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+
+    this.connected = false;
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(dbName);
+        this.connected = true;
+      })
+      .catch((err) => {
+        console.log(`Failed to connect to mongodb: ${err}`);
+      });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;

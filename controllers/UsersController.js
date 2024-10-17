bc9526919a598acd48b985bafdc0e8f842ea4ad5
Antoniomary <amanso.totti@yahoo.com>
2024-10-17 17:34:19 +0000

    UsersController.js

---
+++ controllers/UsersController.js
@@ -0,0 +1,11 @@
+import dbClient from '../utils/db';
+
+class UsersController {
+  static postNew(req, res) {
+    const { email, password } = req.body;
+
+    req.body
+  }
+}
+
+export default UsersController;

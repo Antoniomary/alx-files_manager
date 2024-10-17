74c74f0e62fac439991aebff55d5e00e6e1069b4
Antoniomary <amanso.totti@yahoo.com>
2024-10-17 22:25:23 +0000

    created the UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,31 @@
 import dbClient from '../utils/db';
+import sha1 from 'sha1';
 
 class UsersController {
-  static postNew(req, res) {
+  static async postNew(req, res) {
     const { email, password } = req.body;
 
-    req.body
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
+
+    if (!dbClient.isAlive()) {
+      return res.status(500).json({ error: 'unable to process request' });
+    }
+
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) return res.status(400).json({ error: 'Already exist' });
+
+    const newUser = {
+      email: email,
+      password: sha1(password),
+    };
+
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+
+    res.status(201).json({
+      id: result.insertedId,
+      email: email,
+    });
   }
 }
 
